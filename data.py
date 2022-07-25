5998fb6dc2f9064b44ba08c955ca7c1737ef88bf
SAIF ALI <80207551+saifalisew1508@users.noreply.github.com>
2022-07-25 17:53:37 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,52 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    generate_single_button = [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+
+    home_buttons = [
+        generate_single_button,
+        [InlineKeyboardButton(text="Return", callback_data="home")]
+    ]
+
+    generate_button = [generate_single_button]
+
+    buttons = [
+        generate_single_button,
+        [InlineKeyboardButton("Owner", url="https://t.me/saifalisew1508")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+        ],
+        [InlineKeyboardButton("others bots", url="https://t.me/MissCutieUpdates")],
+    ]
+
+    START = """
+Hey {}
+Welcome to {}
+If you don't trust this bot, 
+1) stop reading this message
+2) delete this chat
+Still reading?
+You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
+By @saifalisew1508
+    """
+
+    HELP = """
+✨ **Available Commands** ✨
+/about - About The Bot
+/help - This Message
+/start - Start the Bot
+/generate - Generate Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
+
+    ABOUT = """
+**About This Bot** 
+Telegram Bot to generate Pyrogram and Telethon string session by @MissCutie_Support
+Source Code : [Click Here](https://github.com/saifalisew1508/StringSessionBot)
+Framework : [Pyrogram](https://docs.pyrogram.org)
+Language : [Python](https://www.python.org)
+Developer : @SAIFALISEW1508
+    """
